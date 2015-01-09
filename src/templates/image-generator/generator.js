d2fbabc1f19a6a6945b7a326da980e7fe03f9edb
gergling <gregmichaeldavies@gmail.com>
2015-01-09 21:10:19 +0000

    Now takes a source image.

--- src/templates/image-generator/generator.js
+++ src/templates/image-generator/generator.js
@@ -20,25 +20,36 @@ module.exports = (function () {
             module: "./",
             generator: "src/templates/image-generator/"
         },
-        distFolderPath = paths.generator + "dist/";
+        distFolderPath = paths.generator + "dist/",
+        generate = function (fnc, png, gen, dst, fileName) {
+            fnc(png, gen);
+            png.pack().pipe(dst);
+            grunt.log.writeln("- Image generated: '" + fileName + "'");
+        };
 
     if (!grunt.file.isDir(distFolderPath)) {
         grunt.file.mkdir(distFolderPath);
     }
 
+    
     grunt.file.expand(paths.generator + "src/*.js").forEach(function (srcPath) {
         var fileName = pathService.basename(srcPath, ".js"),
             distPath = distFolderPath + fileName + ".png",
+            srcImagePath = paths.generator + "src/" + fileName + ".png",
+            srcImageStream,
             dst = fs.createWriteStream(distPath),
             png = newPNG(),
             fnc = require(paths.module + "src/" + fileName),
             gen = new Generator(png);
 
+        // Check if file exists: fs.createReadStream(paths.generator + "src/" + fileName + ".png")
         if (typeof fnc === "function") {
-            fnc(png, gen);
-
-            png.pack().pipe(dst);
-            grunt.log.writeln("- Image generated: '" + fileName + "'");
+            if (grunt.file.exists(srcImagePath)) {
+                grunt.log.writeln("- Reading from source image: '" + srcImagePath + "'");
+                fs.createReadStream(srcImagePath).pipe(png).on('parsed', function () {generate(fnc, png, gen, dst, fileName); });
+            } else {
+                generate(fnc, png, gen, dst, fileName);
+            }
         } else {
             grunt.log.writeln("- No function exported for: "
                 + fileName + ".js");
