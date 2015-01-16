6acc6089c147363acc764e74ca1de22c10b55ab5
gergling <gregmichaeldavies@gmail.com>
2015-01-16 08:28:08 +0000

    Line and circle functions. Deep extend requirement included.

--- src/templates/image-generator/generator/raster.js
+++ src/templates/image-generator/generator/raster.js
@@ -1,7 +1,8 @@
 module.exports = function (png) {
     "use strict";
 
-    var maths = require("./maths"),
+    var extend = require("deep-extend"),
+        maths = require("./maths"),
         Pixel = require("./pixel");
 
     this.pixel = function (x, y, pixel) {
@@ -43,6 +44,52 @@ module.exports = function (png) {
                 pixel.toPNG(png.data, channel);
             }
         }
+    };
+
+    this.line = function (fnc, fromX, fromY, toX, toY) {
+        var dx = toX - fromX,
+            dy = toY - fromY,
+            distance = maths.pythagoras(dx, dy),
+            pixel,
+            i,
+            x,
+            y;
+
+        for (i = 0; i < distance; i += 1) {
+            x = Math.floor(maths.interpolate(fromX, toX, 0, distance, i));
+            y = Math.floor(maths.interpolate(fromY, toY, 0, distance, i));
+            pixel = this.pixel(x, y);
+            fnc(pixel, x, y, i);
+            this.pixel(x, y, pixel);
+        }
+        return distance;
+    };
+    this.circle = function (x, y, radius, fnc, options) {
+        var dx, dy, i, angle, pixel;
+
+        options = extend({
+            radial: {
+                start: 0,
+                arc: 2 * Math.PI
+            }
+        }, options || { });
+        //maths.pythagoras(
+        //pyth = x^2 + y^2 = r^2
+        //for (dx = -radius; dx < radius; dx += 1) {
+            //dy = Math.sqrt(Math.pow(r, 2) - Math.pow(dx, 2));
+            
+        //}
+        // Find out how many pixels in the circumference, then loop those.
+            // It's going to be 2 * pi * radius
+        for (i = 0; i < options.radial.arc * radius; i+= 1) {
+            angle = i / radius;
+            dx = radius * Math.sin(angle + options.radial.start);
+            dy = radius * Math.cos(angle + options.radial.start);
+            pixel = this.pixel(dx, dy);
+            fnc(pixel, dx, dy, angle, i);
+            this.pixel(dx, dy, pixel);
+        }
+        return this;
     };
 
     this.gradient = function (start, end, fromX, fromY, toX, toY) {
