6f29c1a25a3f10805bef6eb1c3d70f69a81817fa
gergling <gregmichaeldavies@gmail.com>
2015-01-17 16:27:30 +0000

    Included isometric library.

--- src/templates/image-generator/generator/core.js
+++ src/templates/image-generator/generator/core.js
@@ -1,9 +1,12 @@
 module.exports = function (png) {
     "use strict";
 
-    var Raster = require("./raster");
+    var Raster = require("./raster"),
+        Isometric = require("./isometric");
 
     this.Pixel = require("./pixel");
 
     this.raster = new Raster(png);
+
+    this.isometric = new Isometric(png, this.raster);
 };
