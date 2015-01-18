3dee200fea763aafd77e96fc828bb8d41d9bf36d
gergling <gregmichaeldavies@gmail.com>
2015-01-18 19:31:23 +0000

    Initial cylinder.

--- src/templates/image-generator/generator/isometric.js
+++ src/templates/image-generator/generator/isometric.js
@@ -5,52 +5,56 @@ module.exports = function (png, raster) {
         Pixel = require("./pixel");
 
     this.ellipse = function (x0, y0, radius, fnc) {
-        var circle = false,
-            isCirclePixel = {
-                ascending: function (x, y) {
-                    var dx = x - x0,
-                        dy = y - y0;
+        var //circle = false,
+            isCirclePixel = function (x, y, positive) {
+                var affirm = false,
+                    dx = x - x0,
+                    dy = y - y0;
 
-                    if (Math.floor(maths.pythagoras(dx, (dy + (dx / 2)))) === radius) {
-                        circle = true;
-                    }
-                    return circle;
-                },
-                descending: function (x, y) {
-                    var dx = x - x0,
-                        dy = y - y0;
-
-                    if (Math.floor(maths.pythagoras(dx, (dy - (dx / 2)))) === radius) {
-                        circle = true;
-                    }
-                    return circle;
+                if (Math.floor(maths.pythagoras(dx, (dy + (dx * positive / 2)))) === radius) {
+                    affirm = true;
                 }
+                return affirm;
+            },
+            directions = {
+                "ascending" : 1,
+                "descending" : -1,
             },
             filterCirclePixels = function (direction) {
                 raster.forEach(function (p, x, y) {
-                    circle = false;
-                    if (isCirclePixel[direction](x, y)) {fnc(p, x, y, direction);}
+                    //circle = false;
+                    if (isCirclePixel(x, y, directions[direction])) {fnc(p, x, y, direction);}
                 });
-            };
+            },
+            ret = { };
 
-        //if (Math.floor(maths.pythagoras(dx, (dy + (dx / 2)))) === radius) { //ascending
-        //if (Math.floor(maths.pythagoras(dx, (dy - (dx / 2)))) === radius) { //descending
-            //console.log(x, y, dx + dy);
-            //circle = true;
-        //}
-        //return circle;
-        return {
-            ascending: function () {filterCirclePixels("ascending"); },
-            descending: function () {filterCirclePixels("descending"); }
-        };
+        Object.keys(directions).forEach(function (direction) {
+            ret[direction] = function () {filterCirclePixels(direction); };
+        });
+
+        return ret;
     };
 
     this.cylinder = function (x0, y0, radius, length, fnc) {
         // Extend lines along the length of the cylinder.
         // Runs a circle routine, each pixel has gradient/pythagoran 
             // co-ordinates generated.
-        return this.ellipse(x0, y0, radius, function (p, x, y) {
-            
+
+        var flags = { };
+
+        return this.ellipse(x0, y0, radius, function (p, x, y, direction) {
+            //console.log(direction);
+            var lineFnc = function (pixel, x, y, i) {
+                    //fnc(p, x, y, direction, i);
+                    pixel.set(p.copy());
+                },
+                gradient = (direction === "ascending" ? -1 : 1),
+                x2 = x + (length * gradient),
+                y2 = y - Math.floor(length / 2);
+
+            p.random();
+            raster.line(lineFnc, x, y, x2, y2);
+            raster.line(lineFnc, x, y + 1, x2, y2 + 1);
         });
     };
 };
