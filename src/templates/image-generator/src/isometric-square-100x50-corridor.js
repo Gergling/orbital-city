e5e89a9eb85e03931a740113a0583ee646771d9a
gergling <gregmichaeldavies@gmail.com>
2015-01-23 00:03:32 +0000

    Limiting code to 4 cylinder tests.

--- src/templates/image-generator/src/isometric-square-100x50-corridor.js
+++ src/templates/image-generator/src/isometric-square-100x50-corridor.js
@@ -6,49 +6,53 @@ module.exports = function (png, generator) {
         i,
         x,
         y,
-        pixel = new Pixel(255, 1, 1, 1),
-        circlePixel = function (x0, y0, radius, x, y) {
-            var circle = false,
-                dx = x - x0,
-                dy = y - y0;
-
-            //if (Math.floor(maths.pythagoras(dx, (dy + (dx / 2)))) === radius) { //ascending
-            //if (Math.floor(maths.pythagoras(dx, (dy - (dx / 2)))) === radius) { //descending
-            var dev = maths.pythagoras(dx, dy * 2);
-            if (dev > radius - 1 && dev < radius) {
-                circle = true;
-            }
-            return circle;
-        };
+        pixel = new Pixel(255, 1, 1, 1);
 
     png.width = 100;
-    png.height = 50;
+    png.height = 100;
 
     // Transparent background
     generator.raster.forEach(function (p, x, y) {
         p.grey(255);
-        if (circlePixel(40, 20, 15, x, y)) {
-            //p.green = 0;
-        }
-        // Filter out the pixels in the circle locations,
-        // skewing by ascending gradient.
     });
-    //generator.raster.pixel(40, 20, new Pixel(1,1,1,255));
 
-    var ellipse = generator.isometric.ellipse(40, 20, 15, function (p, x, y) {
-        p.green = 0;
-    });
-    ellipse.ascending();
-    //generator.raster.circle(40, 20, 15, function (p) {
-        //p.red = 0;
-    //});
-    /*generator.raster.circle(40, 20, 15, function (p, dx, dy) {
-        //var pixel = generator.raster.pixel(dx, dy);
+    generator.isometric.cylinder(20, 20, 15, 20, function (p, x, y) {
+        // Red - Partial circle I want
         p.green = 0;
         p.blue = 0;
-        //generator.raster.pixel(dx, dy, pixel);
-
-        //p.red = 0;
-    });*/
+    }, {
+        circle: {
+            radial: {
+                start: Math.PI + (Math.PI / 8),
+                arc: Math.PI + (Math.PI * 5 / 12)
+            }
+        }
+    });
+    generator.isometric.cylinder(70, 20, 15, 20, function (p, x, y) {
+        // Green - full circle
+        p.red = 0;
+        p.blue = 0;
+    });
+    generator.isometric.cylinder(20, 70, 15, 20, function (p, x, y) {
+        // Purple - full circle
+        p.green = 0;
+    }, {
+        circle: {
+            radial: {
+                start: Math.PI
+            }
+        }
+    });
+    generator.isometric.cylinder(70, 70, 15, 20, function (p, x, y) {
+        // Blue - half circle
+        p.red = 0;
+        p.green = 0;
+    }, {
+        circle: {
+            radial: {
+                arc: Math.PI
+            }
+        }
+    });
 
 };
