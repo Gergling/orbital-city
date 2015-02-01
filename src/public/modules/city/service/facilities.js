829f1f6672576612cacf8ecac8dfa1487fe705a5
gergling <gregmichaeldavies@gmail.com>
2015-02-01 11:33:43 +0000

    Technopedia improvements and superfluous code removal.

---
+++ src/public/modules/city/service/facilities.js
@@ -0,0 +1,14 @@
+angular.module("city").service("city.service.facilities", [
+
+    // Specific facility services.
+
+    function () {
+        "use strict";
+
+        // The hull should probably be built before anything else can be built.
+        // This will be required for MOST facilities.
+        // There may be examples where no hull would be needed.
+        // Other facilities would be residential/industrial.
+        // Facilities are generated from the backend.
+    }
+]);
