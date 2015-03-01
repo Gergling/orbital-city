0b77610f8f08eae4c76b299cf53a3c29b76d2ade
gergling <gregmichaeldavies@gmail.com>
2015-03-01 12:43:13 +0000

    Close button.

---
+++ src/public/modules/application/directive/close.js
@@ -0,0 +1,8 @@
+angular.module("application").directive("close", function () {
+    "use strict";
+
+    return {
+        scope: { },
+        templateUrl: "modules/application/partial/directive-close.html"
+    };
+});
